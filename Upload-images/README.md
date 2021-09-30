ef96c667fd1de49f2f88cb2443c4ac1dd7c5558c
Sammynauta <samuka.o.diogo@gmail.com>
2021-09-30 11:44:55 -0300

    atuliazado e concluido

---
+++ Upload-images/README.md
@@ -0,0 +1,19 @@
+<h1 align="center">Preview upload images</h1>
+
+![Design preview  Preview upload images ](/Upload-images/design/preview.png)
+
+<hr>
+
+- Neste projeto javacript, criei uma caixa de pre-visualizaçao onde o usuario poderá ver as imagens antes de fazer o upload. um projeto basico para aprender a manipular objetos da pagina. Espero que curta.
+
+## Recursos
+
+- [x] HTML,CSS, JS
+
+## Start Dev Environment for VSCode
+
+1. Open project folder in `VSCode`
+2. Install `Live Server`
+3. Right click `index.html` > `Open with Live Server`
+
+![Design preview  Preview upload images ](/Upload-images/design/preview2.png)
--- Upload-images/js/script.js
+++ Upload-images/js/script.js
@@ -1,5 +1,8 @@
 const defaultBtn = document.querySelector("#default-btn"),
+    filename = document.querySelector(".file-name"),
     btn = document.querySelector("button"),
+    closeBtn = document.querySelector("#close"),
+    caixa = document.querySelector(".caixa"),
     img = document.querySelector("img");
 
 function defaultbtnAtivo() {
@@ -8,21 +11,21 @@ function defaultbtnAtivo() {
 
 defaultBtn.addEventListener("change", function () {
     const file = this.files[0];
-    reader.onload = function() {
-        const result = reader.result;
-        img.src = result;
-    }
-    reader.readAsDataURL(file);
-});
-
-/*
-
-     if (file) {
+    if (file) {
         const reader = new FileReader();
         reader.onload = function () {
             const result = reader.result;
             img.src = result;
+            caixa.classList.add("active")
         }
+        closeBtn.addEventListener("click", function() {
+            img.src = "";
+            caixa.classList.remove("active");
+        })
         reader.readAsDataURL(file)
     }
-*/
+    if(this.value) {
+        let valueStore = this.value;
+        filename.textContent = valueStore;
+    }
+});
