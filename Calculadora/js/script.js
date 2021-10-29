4d1d6110993451d731be47b3b87964ced61885b9
Sammynauta <samuka.o.diogo@gmail.com>
2021-10-29 13:47:07 -0300

    funcionalidade concluida.

--- Calculadora/js/script.js
+++ Calculadora/js/script.js
@@ -14,3 +14,13 @@ function Apagar() {
     let resultado = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length - 1);
 }
+
+function calcular() {
+    let resultado = document.getElementById('resultado').innerHTML;
+    if (resultado) {
+        document.getElementById('resultado').innerHTML = eval(resultado);
+    } else {
+        document.getElementById('resultado').innerHTML = "Nada...";
+        return setTimeout(() => {Limpar();}, 1000);
+    }
+}
